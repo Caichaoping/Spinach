d88d2c6e2c39d75520f27effc87af4432a06d6db
Caichaoping <971859818@qq.com>
2016-03-21 17:23:42 +0800

    Log

--- app/src/main/java/com/cc/spinach/ui/MainActivity.java
+++ app/src/main/java/com/cc/spinach/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.cc.spinach.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.cc.spinach.R;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Log.d("cc","");
 
     }
 }
