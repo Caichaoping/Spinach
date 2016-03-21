5e7689b8fc214c02bab5aa03525db9ebda164de9
Caichaoping <971859818@qq.com>
2016-03-21 18:38:44 +0800

    Log

--- app/src/main/java/com/cc/spinach/ui/MainActivity.java
+++ app/src/main/java/com/cc/spinach/ui/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Log.d("cc","");
+        Log.d("cc","vv");
 
     }
 }
---
+++ app/src/main/java/com/cc/spinach/ui/NewDetailsActivity.java
@@ -0,0 +1,33 @@
+package com.cc.spinach.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.cc.spinach.R;
+
+/**
+ * 注释：
+ * 作者：菠菜 on 2016/3/21 17:48
+ * 邮箱：971859818@qq.com
+ */
+public class NewDetailsActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newdetails);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
